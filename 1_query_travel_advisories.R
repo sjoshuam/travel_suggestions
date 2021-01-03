54022e006519ce70053550bbffd8a04ad02102f8
Josh M <70724269+sjoshuam@users.noreply.github.com>
2021-01-03 11:18:52 -0500

    Retrieve State Dept advisories and refine data

---
+++ 1_query_travel_advisories.R
@@ -0,0 +1,69 @@
+##########==========##########==========##########==========##########==========
+
+## SET-UP ==========
+
+## Meta-information
+## Author: Joshua Mendelsohn
+## Creation: 2020-01-03
+## Version: 4.0.3
+## Description: retrieve current travel advisories from state department website
+
+## environment set-up
+remove(list = objects())
+options(width = 80, scipen = 2, digits = 6)
+library(lintr) #lint("1_query_travel_advisories.R")
+library(rvest)
+library(tidyverse)
+
+
+## QUERY STATE DEPT RSS FEED FOR ADVISORIES ==========
+
+## read in RSS feed
+dos_rss <- read_html("https://travel.state.gov/_res/rss/TAsTWs.xml") 
+
+## PARSE ADVISORIES ==========
+
+## extract level/country data
+dos_level <- dos_rss %>%
+  html_nodes("item") %>%
+  html_nodes("title") %>%
+  html_text()
+
+## parse country and level
+dos_level <- dos_level %>%
+  str_split(pattern = " - ") %>%
+  simplify2array() %>%
+  t()
+
+## convert to tibble format
+colnames(dos_level) <- c("country", "level")
+dos_level <- as_tibble(dos_level) %>%
+  mutate("level_num" =  str_replace_all(level, "[^0-9]", ""))
+
+dos_level$level_num <- as.integer(dos_level$level_num)
+
+## PARSE JUSTIFICATIONS ==========
+
+## extract justification (for level) data
+dos_reason <- dos_rss %>%
+  html_nodes("item") %>%
+  html_nodes("description") %>%
+  html_text() %>%
+  str_replace("due\\sto\\s", ". ") %>%
+  str_split("\\.\\s") %>%
+  map_chr(nth, 2) %>%
+  str_replace("\\s", " ") %>%
+  str_replace("(^\\s+)|(\\s+$)", "") %>%
+  enframe(value= "reasons")
+
+## incorporate into primary data object
+dos_advice <- bind_cols(dos_level, select(dos_reason, reasons)) %>%
+  mutate("reasons" = if_else(level_num == 1, "(none)", reasons)) %>%
+  mutate("covid_adj"= if_else(reasons == "COVID-19", level_num - 1L, level_num))
+remove(dos_reason, dos_level)
+
+## EXPORT FINAL DATASET ==========
+saveRDS(dos_advice, file= "B_Intermediates/dos_advice.RData")
+
+
+##########==========##########==========##########==========##########==========
