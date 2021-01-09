d44b0990c0742f557787a9df2a5dcf3d3c50e0f3
Josh M <70724269+sjoshuam@users.noreply.github.com>
2021-01-09 10:50:21 -0500

    Add database of common languages

--- 1_query_travel_advisories.R
+++ 1_query_travel_advisories.R
@@ -11,14 +11,18 @@
 ## environment set-up
 remove(list = objects())
 options(width = 80, scipen = 2, digits = 6)
+library(jsonlite)
 library(rvest)
 library(tidyverse)
-
 
 ## QUERY STATE DEPT RSS FEED FOR ADVISORIES ==========
 
 ## read in RSS feed
-dos_rss <- read_html("https://travel.state.gov/_res/rss/TAsTWs.xml")
+dos_rss <- read_html(url("https://travel.state.gov/_res/rss/TAsTWs.xml"))
+factbook_json <- fromJSON(url(paste0(
+  "https://raw.githubusercontent.com/",
+  "iancoleman/cia_world_factbook_api/master/data/factbook.json")
+  ))
 
 ## PARSE ADVISORIES ==========
 
@@ -65,8 +69,54 @@ remove(dos_reason, dos_level)
 dos_advice <- dos_advice %>%
   add_row(country = "USA", level_num = 1, covid_adj = 1, reasons = "(none)") %>%
   mutate(country = str_replace(country, "^Israel.*", "Israel"))
-  
+
+## DOWNLOAD JSON-SCRAPE OF THE CIA WORLD FACTBOOK ==========
+
+## extract language data from json object
+extract_language <- function(x) {
+  x <- x$data$people$languages$language
+  if (class(x) != "data.frame") {x <- data.frame(
+    "name" = NA, "percent" = NA, "note" = NA)}
+  if (!("note" %in% colnames(x))) {x$note <- as.character(NA)}
+  if (!("percent" %in% colnames(x))) {x$percent <- as.character(NA)}
+  x[, c("name", "percent", "note")]
+  }
+factbook_json <- factbook_json$countries
+factbook_json <- lapply(factbook_json, extract_language)
+factbook_json <- data.frame(
+  "country" = rep(names(factbook_json), sapply(factbook_json, nrow)),
+  do.call(what = rbind, args = factbook_json)
+  )
+factbook_json <- as_tibble(factbook_json)
+
+## repair missing data
+language_missing <- tibble(
+  "official" = str_detect(factbook_json$note, "official"),
+  "auxiliary" = str_detect(factbook_json$note, "lingua franca"),
+  ) %>%
+  mutate("official"  = if_else(is.na(official), FALSE, official)) %>%
+  mutate("auxiliary" = if_else(is.na(auxiliary), FALSE, auxiliary)) %>%
+  mutate("repair" = as.numeric(official | auxiliary)) %>%
+  mutate("country" = pull(factbook_json, country)) %>%
+  mutate("denominator" = tapply(repair, country, sum)[country]) %>%
+  mutate("denominator" = pmax(denominator, 1)) %>%
+  mutate("repair" = (repair / denominator) * 100)
+
+factbook_json <- factbook_json %>%
+  mutate("repair" = language_missing$repair) %>%
+  mutate("percent" = if_else(is.na(percent), repair, as.numeric(percent))) %>%
+  select(-repair) %>%
+  mutate("percent" = round(percent / 100, 2)) %>%
+  filter(!(country %in% c("world", "european_union"))) %>%
+  mutate("country" = str_replace_all(country, "_", " "))
+
+remove(language_missing)
+
 ## EXPORT FINAL DATASET ==========
+
+common_languages <- factbook_json
+
 saveRDS(dos_advice, file = "B_Intermediates/dos_advice.RData")
+saveRDS(common_languages, file = "B_Intermediates/common_languages.RData")
 
 ##########==========##########==========##########==========##########==========
